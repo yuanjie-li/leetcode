da925ac87c6055fd173cbb0a972031ad11f7809e
Yuanjie Li <yuanjie.li@utexas.edu>
2025-02-04 09:00:10 -0700

    Create 620_not_boring_movies.sql

---
+++ SQL50/620_not_boring_movies.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+SELECT 
+    id
+    ,movie
+    ,description 
+    ,rating 
+FROM Cinema 
+WHERE 
+id % 2 = 1 
+AND 
+description <> 'boring'
+ORDER BY rating DESC 
